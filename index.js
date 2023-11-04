f7d364e7abb08cf582c8cb3c2d6f926fb6725260
b0r1slav <borislav.madzharov@gmail.com>
2023-11-04 16:36:30 +0200

    fuck

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
         const $twinFilter = $("#twin-filter");
         const $typeFilter = $("#type-filter");
         let calendar = {};
+        let loaded = false;
 
         function populateTypeFilter(types = []) {
             types.forEach((t) => {
@@ -19,94 +20,89 @@
         }
 
         function loadEvents() {
-            var requestOptions = {
-                method: 'GET',
-                redirect: 'follow'
-            };
-
-            fetch("https://script.google.com/macros/s/AKfycbxNnO6euZTMq_q8EM4k0bvYsO2QTCDZ3yGevR0eSkWCmoTrfsbdEQlPViNxIDH9GY-Ctw/exec", requestOptions)
-                .then(response => response.json())
-                .then((result) => {
-                    $('.container-fluid').css('display', 'block');
-                    $('#loader').css('display', 'none');
-                    
-                    if (result?.success) {
-                        populateTypeFilter(result.types);
-
-                        const calendarEl = document.getElementById('calendar');
-                        calendar = new FullCalendar.Calendar(calendarEl, {
-                            initialView: 'listDay',
-                            locale: "bg-BG",
-                            height: "auto",
-                            headerToolbar: {
-                                left: 'prev,next',
-                                center: 'title',
-                                right: 'listDay,dayGridWeek,listWeek,dayGridMonth'
-                            },
-                            buttonText: {
-                                listDay: 'Днес лист',
-                                listWeek: 'Седмица лист',
-                                dayGridWeek: 'Седмица',
-                                dayGridMonth: 'Месец',
-                            },
-                            eventTimeFormat: {
-                                hour: '2-digit',
-                                minute: '2-digit',
-                                hour12: false
-                            },
-                            datesSet: (dateInfo) => {
-                                console.log(dateInfo);
-                            },
-                            events: function (fetchInfo, successCallback, failureCallback) {
-                                successCallback(result.events);
-                            },
-                            eventDidMount: function (info) {
-                                if (info.backgroundColor) {
-                                    info.el.style.background = info.backgroundColor;
-                                }
-
-                                if (info.event.extendedProps.nutrient) {
-                                    const $badge = $('<span></span>').addClass('badge ms-2')
-                                        .text(info.event.extendedProps.nutrient)
-                                        .css('backgroundColor', info.event.extendedProps.nutrientColor);
-                                    
-                                    appendElToEvent(info.el, $badge);
-                                }
-
-                                if (info.event.extendedProps.quantity) {
-                                    const $quantity = $('<span></span>').addClass('ms-2 fw-bold')
-                                        .text(info.event.extendedProps.quantity);
-                                    
-                                    appendElToEvent(info.el, $quantity);
-                                }
-
-                                if (info.event.extendedProps.comment) {
-                                    const $comment = $('<span></span>').addClass('ms-2 fst-italic')
-                                        .text(info.event.extendedProps.comment);
-
-                                    appendElToEvent(info.el, $comment);
-                                }
-
-                                const twinName = info.event.extendedProps.twinName.toLowerCase();
-                                if ($twinFilter.val() && twinName !== $twinFilter.val().toLowerCase()) {
-                                    info.el.style.display = 'none';
-                                }
-
-                                const typeFilter = Array.isArray($typeFilter.val()) ? $typeFilter.val() : [];
-                                const typeFilterValues = typeFilter.map(el => el.toLowerCase());
-                                const type = info.event.extendedProps.type.toLowerCase();
-
-                                if (typeFilterValues.length) {
-                                    if (!typeFilterValues.includes(type) && !(typeFilterValues.includes("нутриенти") && info.event.extendedProps.nutrient)) {
-                                        info.el.style.display = 'none';
-                                    }
-                                }
-                            }
-                        });
-                        calendar.render();
+            const calendarEl = document.getElementById('calendar');
+            calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'listDay',
+                locale: "bg-BG",
+                height: "auto",
+                headerToolbar: {
+                    left: 'prev,next',
+                    center: 'title',
+                    right: 'listDay,dayGridWeek,listWeek,dayGridMonth'
+                },
+                buttonText: {
+                    listDay: 'Днес лист',
+                    listWeek: 'Седмица лист',
+                    dayGridWeek: 'Седмица',
+                    dayGridMonth: 'Месец',
+                },
+                eventTimeFormat: {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    hour12: false
+                },
+                loading: (isLoading) => {
+                    if (isLoading) {
+                        $('.container-fluid').css('display', 'none');
+                        $('#loader').css('display', 'block');
+                    } else {
+                        $('.container-fluid').css('display', 'block');
+                        $('#loader').css('display', 'none');
                     }
-                })
-                .catch(error => console.log('error', error));
+                },
+                eventSourceSuccess: function (content) {
+                    if (!loaded) {
+                        populateTypeFilter(content.types);
+                        loaded = true;
+                    }
+
+                    return content.events;
+                },
+                events: "https://script.google.com/macros/s/AKfycbxNnO6euZTMq_q8EM4k0bvYsO2QTCDZ3yGevR0eSkWCmoTrfsbdEQlPViNxIDH9GY-Ctw/exec",
+                eventDidMount: function (info) {
+                    if (info.backgroundColor) {
+                        info.el.style.background = info.backgroundColor;
+                    }
+
+                    if (info.event.extendedProps.nutrient) {
+                        const $badge = $('<span></span>').addClass('badge ms-2')
+                            .text(info.event.extendedProps.nutrient)
+                            .css('backgroundColor', info.event.extendedProps.nutrientColor);
+
+                        appendElToEvent(info.el, $badge);
+                    }
+
+                    if (info.event.extendedProps.quantity) {
+                        const $quantity = $('<span></span>').addClass('ms-2 fw-bold')
+                            .text(info.event.extendedProps.quantity);
+
+                        appendElToEvent(info.el, $quantity);
+                    }
+
+                    if (info.event.extendedProps.comment) {
+                        const $comment = $('<span></span>').addClass('ms-2 fst-italic')
+                            .text(info.event.extendedProps.comment);
+
+                        appendElToEvent(info.el, $comment);
+                    }
+
+                    const twinName = info.event.extendedProps.twinName.toLowerCase();
+                    if ($twinFilter.val() && twinName !== $twinFilter.val().toLowerCase()) {
+                        info.el.style.display = 'none';
+                    }
+
+                    const typeFilter = Array.isArray($typeFilter.val()) ? $typeFilter.val() : [];
+                    const typeFilterValues = typeFilter.map(el => el.toLowerCase());
+                    const type = info.event.extendedProps.type.toLowerCase();
+
+                    if (typeFilterValues.length) {
+                        if (!typeFilterValues.includes(type) && !(typeFilterValues.includes("нутриенти") && info.event.extendedProps.nutrient)) {
+                            info.el.style.display = 'none';
+                        }
+                    }
+                }
+            });
+            calendar.render();
         }
 
         loadEvents();
@@ -127,7 +123,7 @@
             const val = $tf.val();
             const clicked = e.currentTarget.value;
             const fVal = (val || []).filter(v => v !== clicked);
-            
+
             $tf.val(fVal).trigger('change');
         });
     });
